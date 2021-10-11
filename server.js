f8b55df2f96e252ac3f967b746fb385f14bb7ca8
Elziekio <anthony.deamer@hotmail.com>
2021-10-11 15:34:32 +1000

    deploying

--- server.js
+++ server.js
@@ -19,7 +19,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connecting moogoose to the port
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // Requiring routes
 require("./routes/apiRoutes")(app);
