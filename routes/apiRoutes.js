bec72d0c244f68e6a8cfa493c0cafd8c81b94226
Elziekio <anthony.deamer@hotmail.com>
2021-10-11 12:59:45 +1000

    Adding if statement for error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,19 @@ module.exports = function(app) {
     // Get request for all workouts
     app.get("/api/workouts", (req, res) => {
         Workout.find().then(function(data) {
-            console.log(data);
+            if(error) {
+                res.send(error);
+            } else{
+                console.log(data);
+                res.json(data);
+            }
+        });
+    });
+
+    // Add a new workout
+    app.post("api/workouts", (req,res) => {
+        console.log(req.body);
+        Workout.create({}).then(function(data) {
             res.json(data);
         });
     });
